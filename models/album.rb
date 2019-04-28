91209b713c0d31ad7e282b7a620e39f3d5419a91
masanariyamamoto <masa.060419@gmail.com>
2019-04-28 21:47:12 +0900

    modify db

--- models/album.rb
+++ models/album.rb
@@ -1,3 +1,8 @@
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||"sqlite3:db/development.db")
 class Album < ActiveRecord::Base
+    belongs_to :category
+end
+
+class Category < ActiveRecord::Base
+    has_many :albums
 end
